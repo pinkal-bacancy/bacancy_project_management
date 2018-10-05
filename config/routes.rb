9408d0fdda043c6587acfd0720747165a6f7b3d9
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-10-05 10:56:27 +0530

    added unique validation in employee and mailer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
 
-  resources :employees
+  resources :employees,path: 'employee'
+  get "/unique_employee" => "employees#unique_employee"
+  get "/unique_username" => "employees#unique_username"
   resources :projects
   resources :assignments
   resources :clients
